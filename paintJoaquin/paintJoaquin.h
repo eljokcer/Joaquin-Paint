b98e5a6245a1e33f02ee10fa5087c8f0565a0406
joaquin cuneo <joaquincuneo@hotmail.com>
2019-04-06 11:27:19 +0300

    Add project files.

---
+++ paintJoaquin/paintJoaquin.h
@@ -0,0 +1,32 @@
+﻿
+// paintJoaquin.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CpaintJoaquinApp:
+// See paintJoaquin.cpp for the implementation of this class
+//
+
+class CpaintJoaquinApp : public CWinApp
+{
+public:
+	CpaintJoaquinApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CpaintJoaquinApp theApp;
---
+++ paintJoaquin/paintJoaquinDlg.h
@@ -0,0 +1,34 @@
+﻿
+// paintJoaquinDlg.h : header file
+//
+
+#pragma once
+
+
+// CpaintJoaquinDlg dialog
+class CpaintJoaquinDlg : public CDialogEx
+{
+// Construction
+public:
+	CpaintJoaquinDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_PAINTJOAQUIN_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
